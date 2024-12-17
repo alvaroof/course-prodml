dfa9eb9dfa4f59840a4990085a7b62d94059c737
Ortiz Fernandez <ORTIZAL9@novartis.net>
2024-12-17 09:23:47 +0100

    Implementing logging through loguru

--- 0_alvaro_implementation/config.py
+++ 0_alvaro_implementation/config.py
@@ -1,15 +1,21 @@
-# data_file_name - path to our csv file in collection
+# data_file_name - path to our csv file in collection.py
 # model_path - the folder containing model config files
 # model_name - the name of the model we should use
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import DirectoryPath, FilePath
 
+from loguru import logger 
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
     data_file_name: FilePath
     model_path: DirectoryPath
     model_name: str
-    
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    log_level: str
 
 settings = Settings()
+
+logger.remove()
+logger.add("app.log", rotation="1 day", retention="2 days", compression="zip", level=settings.log_level)
--- 0_alvaro_implementation/model.py
+++ 0_alvaro_implementation/model.py
@@ -3,9 +3,13 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 import pickle as pk
 from preparation import prepare_data
+from loguru import logger
+
 from config import settings
 
 def build_model():
+    
+    logger.info("starting up model building pipeline")
     # to train and then save the model we need:
 
     #1. load preprocessed dataset
@@ -17,8 +21,7 @@ def build_model():
     #4. train the model
     rf = train_model(X_train, y_train)
     #5. evaluate the model
-    score = evaluate_model(rf, X_test, y_test)
-    print(f'Models score = {score}')
+    evaluate_model(rf, X_test, y_test)
     #6. save the model in a configuration file
     save_model(rf)
 
@@ -33,10 +36,14 @@ def get_X_y(data,
                   'garage_yes', 
                   'storage_yes'],
             col_y = 'rent'):
+    
+    logger.info(f"defining X and Y variables. \nX vars: {col_X}\ny var: {col_y}")
 
     return data[col_X], data[col_y]
 
 def split_train_test(X, y):
+
+    logger.info("splitting data into train and test sets")
     X_train, X_test, y_train, y_test = train_test_split(X, 
                                                         y, 
                                                         test_size=0.2)
@@ -45,8 +52,12 @@ def split_train_test(X, y):
 
 def train_model(X_train, y_train):
 
+    logger.info("training a model with hyperparameters")
+
     grid_space = {'n_estimators': [100, 200, 300], 
                   'max_depth': [3, 6, 9, 12]}
+    
+    logger.debug(f"grid_space = {grid_space}")
     
     grid = GridSearchCV(RandomForestRegressor(), 
                         param_grid=grid_space, 
@@ -58,9 +69,13 @@ def train_model(X_train, y_train):
     return model_grid.best_estimator_
 
 def evaluate_model(model, X_test, y_test):
+
+    logger.info(f"evaluating model performance. SCORE={model.score(X_test, y_test)}")
     return model.score(X_test, y_test)
 
 def save_model(model):
-    import os
-    os.makedirs(f'{settings.model_path}', exist_ok=True)
-    pk.dump(model, open(f'{settings.model_path}/{settings.model_name}', 'wb'))
+
+    logger.info(f"saving a model to a directory: {settings.model_path}/{settings.model_name}")
+    pk.dump(model, open(f'{settings.model_path}/{settings.model_name}', 'wb')) #model_path + model_name
+
+
