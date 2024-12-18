e9030afa5a4879df0d96280ffabd1ccccefb55f3
Alvaro Ortiz Fernandez <alvaro.ortiz_fernandez@novartis.com>
2024-12-18 16:19:28 +0100

    Clean code principles (SOLID)

--- 0_alvaro_implementation/src/model/pipeline/collection.py
+++ 0_alvaro_implementation/src/model/pipeline/collection.py
@@ -1,17 +1,31 @@
-import pandas as pd
-from config.config import settings
-from loguru import logger
+"""
+This module provides functionalities to load data from a database.
 
-from config.config import engine
-from db.db_model import RentApartments
+It includes a function to extract data from the RentApartments table
+in the database and load it into a pandas DataFrame. This module is useful
+for scenarios where data needs to be retrieved from a database for further
+analysis or processing. It uses SQLAlchemy for executing database queries
+and pandas for handling the data in a DataFrame format.
+"""
+
+import pandas as pd
+from loguru import logger
 from sqlalchemy import select
 
+from config import engine
+from db.db_model import RentApartments
 
-def load_data(path): #data_file_name
-    logger.info(f"loading csv file at path {path}")
-    return pd.read_csv(path)
 
-def load_data_from_db():
-    logger.info("Extracting data from database")
+def load_data_from_db() -> pd.DataFrame:
+    """
+    Extract the entire RentApartments table from the database.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the RentApartments data.
+    """
+    logger.info('extracting the table from the database')
     query = select(RentApartments)
-    return pd.read_sql(query, engine)
+    return pd.read_sql(
+        query,
+        engine,
+    )
--- 0_alvaro_implementation/src/tests/check_executable.py
+++
@@ -1,4 +0,0 @@
-import os, sys
-
-
-print(sys.executable)
